48674d3ff76ef8e4c47dd37e9f93657c19680964
dhairya <119152782+dhairyas1@users.noreply.github.com>
2024-12-09 23:13:14 +0530

    Update auth.js

--- backend-course-prj-master/routes/auth.js
+++ backend-course-prj-master/routes/auth.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const authController = require("../controllers/auth");
 const { check, body } = require("express-validator");
 const extractToken = require("../middleware/extractToken");
 const isAuth = require("../middleware/is-auth");
 const isUserAuth = require("../middleware/is-user-auth");
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
 // PUT signup/
-router.put("/signup", authController.signup);
+router.put(
+  "/signup",
+  [
+    check("email").isEmail().withMessage("Please enter a valid email."),
+    body("password").trim().isLength({ min: 5 })
+  ],
+  authController.signup
+);
 
 // POST login
 router.post("/login", authController.login);
@@ -17,4 +24,3 @@ router.post("/login", authController.login);
 router.post("/logout", extractToken, authController.logout);
 
 module.exports = router;
-
