c8130b8f5d1cf1fc9e6484e9eb597560e95b386a
Dhairya <dhairya.sibal1@gmail.com>
2024-12-20 15:24:09 +0530

    Fix Redux Toolkit versions and RTK Query imports

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import { rtkQueryErrorLogger } from '../middleware/middleware';
 import { courseApi } from '../pages/admin/Courses/course.service';
 import { categoryApi } from '../pages/admin/Categories/category.service';
@@ -35,7 +35,6 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
-  // Thêm api middleware để enable các tính năng như caching, invalidation, polling của rtk-query
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       courseApi.middleware,
@@ -49,12 +48,7 @@ export const store = configureStore({
     )
 });
 
-// Optional, nhưng bắt buộc nếu dùng tính năng refetchOnFocus/refetchOnReconnect
 setupListeners(store.dispatch);
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-
-// Write Hello world function in javascript
