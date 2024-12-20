dbdf1fb1190f7d4a90d4148f25690827c1fbd928
Dhairya <dhairya.sibal1@gmail.com>
2024-12-20 18:07:22 +0530

    Update course links to use .html extension

--- src/pages/site/CourseHome/index.tsx
+++ src/pages/site/CourseHome/index.tsx
@@ -7,13 +7,13 @@ const CourseHome = () => {
       <h1 className="course-title">Available Courses</h1>
       <div className="courses-grid">
         <div className="course-card">
-          <Link to="/course_1" className="course-button">Course 1</Link>
+          <Link to="/course_1.html" className="course-button">Course 1</Link>
         </div>
         <div className="course-card">
-          <Link to="/course_2" className="course-button">Course 2</Link>
+          <Link to="/course_2.html" className="course-button">Course 2</Link>
         </div>
         <div className="course-card">
-          <Link to="/course_3" className="course-button">Course 3</Link>
+          <Link to="/course_3.html" className="course-button">Course 3</Link>
         </div>
       </div>
     </div>
