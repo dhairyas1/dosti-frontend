e6acd63cb1e90046538d8a4f78e8fd3893e73639
Dhairya <dhairya.sibal1@gmail.com>
2024-12-20 17:13:35 +0530

    Add React Router future flags to fix warnings

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,15 @@ document.body.appendChild(notificationRoot);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <RouterProvider 
+        router={router} 
+        future={{
+          v7_fetcherPersist: true,
+          v7_normalizeFormMethod: true,
+          v7_partialHydration: true,
+          v7_skipActionErrorRevalidation: true
+        }}
+      />
     </Provider>
   </React.StrictMode>
 );
